c87fdd260ef158e072e33aaa8dbc55f293b2281b
Janis Kauls <janciskauls@gmail.com>
2023-04-17 18:47:56 +0300

    Initial commit

---
+++ checkers/constants.py
@@ -0,0 +1,14 @@
+import pygame
+
+WIDTH, HEIGHT = 800, 800
+ROWS, COLS = 8, 8
+SQUARE_SIZE = WIDTH//COLS
+
+RED = (35, 146, 164)
+WHITE = (200,200,200)
+BLACK = (0, 0, 0)
+BLUE = (65,202,65)
+GRAY = (65,202,65)
+
+CROWN = pygame.transform.scale(pygame.image.load('checkers/assets/Xletter.png'), (45, 45))
+
---
+++ main.py
@@ -0,0 +1,41 @@
+import pygame
+from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE
+from checkers.game import Game
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Checkers')
+
+def get_row_col_from_mouse(pos):
+    x, y = pos
+    row = y // SQUARE_SIZE
+    col = x // SQUARE_SIZE
+    return row, col
+
+def main():
+    run = True
+    game = Game(WIN)
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(60.0)
+
+        if game.winner() != None:
+            print(game.winner())
+            run = False
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                row, col = get_row_col_from_mouse(pos)
+                game.select(row, col)
+
+        game.update()
+
+    pygame.quit()
+
+main()
+
+
